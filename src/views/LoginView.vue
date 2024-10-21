2971e3c442f3aea0c3da86e172ef6a2075734abd
Kevin Kim <kimanikevin254@gmail.com>
2024-10-21 10:26:39 +0300

    add max size to the auth page

--- src/views/LoginView.vue
+++ src/views/LoginView.vue
@@ -1,6 +1,6 @@
 <script setup>
 import { Descope, useSession } from "@descope/vue-sdk";
-import { ref, watch } from "vue";
+import { watch } from "vue";
 import { useRouter } from "vue-router";
 
 const router = useRouter();
@@ -16,7 +16,7 @@ watch(isAuthenticated, (newValue) => {
 </script>
 
 <template>
-    <div class="h-screen mt-4">
+    <div class="h-screen max-w-xl mt-4 mx-auto">
         <p v-if="isLoading">Loading...</p>
         <Descope v-if="!isAuthenticated" :flowId="'sign-up-or-in'" />
     </div>
